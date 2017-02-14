658c755707abfc0b179c82ba2e22badc6d76fecc
alpri.aditiansyah@iconpln.co.id <alpri.aditiansyah@iconpln.co.id>
2017-02-14 14:46:36 +0700

    list menu controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -43,6 +43,207 @@ class Home extends CI_Controller {
 	public function batal(){
 		$this->load->view('vbatalrestitusi');	
 	}
+
+    public function transToken(){
+        $this->load->view->rekon('vtransToken');  
+    }
+
+    public function kontrol404BlnLalu(){
+        $this->load->view->rekon('vkontrol309BlnLalu');  
+    }
+
+    public function kontrol309BlnLalu(){
+        $this->load->view->rekon('vkontrol309BlnLalu');  
+    }
+
+    public function kontrol406BlnLalu(){
+        $this->load->view->rekon('vkontrol406BlnLalu');  
+    }
+
+    public function lap309GabKumBlnLalu(){
+        $this->load->view->rekon('vlap309GabKumBlnLalu');  
+    }
+
+    public function lap309GabBlnLalu(){
+        $this->load->view->rekon('vlap309GabBlnLalu');  
+    }
+
+    public function 307Hal1(){
+        $this->load->view->rekon('v307Hal1');  
+    }
+
+    public function 307Hal2(){
+        $this->load->view->rekon('v307Hal2');  
+    }
+
+    public function 308(){
+        $this->load->view->rekon('v308');  
+    }
+
+    public function kontrol406(){
+        $this->load->view->rekon('vkontrol406');  
+    }
+
+    public function 406(){
+        $this->load->view->rekon('v406');  
+    }
+
+    public function 406Gab(){
+        $this->load->view->rekon('v406Gab');  
+    }
+
+    public function kontrol404(){
+        $this->load->view->rekon('vkontrol404');  
+    }
+
+    public function kontrol309(){
+        $this->load->view->rekon('vkontrol309');  
+    }
+
+    public function lap309Gab(){
+        $this->load->view->rekon('vlap309Gab');  
+    }
+
+    public function lap309GabKum(){
+        $this->load->view->rekon('vlap309GabKum');  
+    }
+
+    public function lap309Lpb(){
+        $this->load->view->rekon('vlap309Lpb');  
+    }
+
+    public function lap309LpbKum(){
+        $this->load->view->rekon('vlap309LpbKum');  
+    }
+
+    public function lap309pasca(){
+        $this->load->view->rekon('vlap309pasca');  
+    }
+
+    public function lap309pascaKum(){
+        $this->load->view->rekon('vlap309pascaKum');  
+    }
+
+
+    public function lap_pend_LPB_tarif(){
+        $this->load->view->rekon('vlap_pend_LPB_tarif');  
+    }
+
+    public function lap_pend_LPB_kogol(){
+        $this->load->view->rekon('vlap_pend_LPB_kogol');  
+    }
+
+    public function daftarPRRLamp3A(){
+        $this->load->view->rekon('vdaftarPRRLamp3A');  
+    }
+
+    public function daftarPRRLamp3B(){
+        $this->load->view->rekon('vdaftarPRRLamp3B');  
+    }
+
+    public function daftarPRRLamp4A(){
+        $this->load->view->rekon('vdaftarPRRLamp4A');  
+    }
+
+    public function lapPelunasanPRR(){
+        $this->load->view->rekon('vlapPelunasanPRR');  
+    }
+
+    public function lapPenetapanBPDicicil(){
+        $this->load->view->rekon('vlapPenetapanBPDicicil');  
+    }
+
+    public function detailselisihLapSaldoBP(){
+        $this->load->view->rekon('vdetailselisihLapSaldoBP');  
+    }
+
+    public function penetapanNontaglis(){
+        $this->load->view->rekon('vpenetapanNontaglis');  
+    }
+
+    public function pembatalanNontaglis(){
+        $this->load->view->rekon('vpembatalanNontaglis');  
+    }
+
+    public function pembatalanAngNontaglis(){
+        $this->load->view->rekon('vpembatalanAngNontaglis');  
+    }
+
+    public function rekapPPJHal1(){
+        $this->load->view->rekon('vrekapPPJHal1');  
+    }
+
+    public function rekapPPJHal2(){
+        $this->load->view->rekon('vrekapPPJHal2');  
+    }
+
+    public function ppnR3Hal1(){
+        $this->load->view->rekon('vppnR3Hal1');  
+    }
+
+    public function ppnHal2(){
+        $this->load->view->rekon('vppnHal2');  
+    }
+
+    public function 5b1lama(){
+        $this->load->view->rekon('v5b1lama');  
+    }
+
+    public function 5b1baru(){
+        $this->load->view->rekon('v5b1baru');  
+    }
+
+    public function 5b1a(){
+        $this->load->view->rekon('v5b1a');  
+    }
+
+    public function 5b1gab(){
+        $this->load->view->rekon('v5b1gab');  
+    }
+
+    public function 5b2(){
+        $this->load->view->rekon('v5b2');  
+    }
+
+    public function 5b3a(){
+        $this->load->view->rekon('v5b3a');  
+    }
+
+    public function 5b3b(){
+        $this->load->view->rekon('v5b3b');  
+    }
+
+    public function 5b4a(){
+        $this->load->view->rekon('v5b4a');  
+    }
+
+    public function 5b4b(){
+        $this->load->view->rekon('v5b4b');  
+    }
+
+    public function 10j1(){
+        $this->load->view->rekon('v10j1');  
+    }
+
+    public function 10j2(){
+        $this->load->view->rekon('v10j2');  
+    }
+
+    public function 11a1(){
+        $this->load->view->rekon('v11a1');  
+    }
+
+    public function 11a2(){
+        $this->load->view->rekon('v11a2');  
+    }
+
+    public function 11a3(){
+        $this->load->view->rekon('v11a3');  
+    }
+
+    public function 11a4(){
+        $this->load->view->rekon('v11a4');  
+    }
 	
 	public function getdata() {
 		$id = $this->input->post('inNoAgenda');
